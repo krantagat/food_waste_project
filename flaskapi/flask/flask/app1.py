933db135e538ddce1c7944d74204c929f3aac3a4
krantagat <kranta.g@gmail.com>
2024-04-21 02:58:41 +0200

    set_up_api_:))

---
+++ flaskapi/flask/flask/app1.py
@@ -0,0 +1,31 @@
+# Let's write your code here!
+
+
+from flask import Flask
+import requests
+import pymysql
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/foodwastes/")
+# @auth.required
+
+def foodwastes():
+    db_conn = pymysql.connect(host="localhost", user="root"
+                              ,password=os.getenv('kranta_sql_password')
+                              , database="waste_less_taste"
+                              ,cursorclass=pymysql.cursors.DictCursor)
+    with db_conn.cursor() as cursor:
+        cursor.execute("""
+                        SELECT 
+                       * from tonne_cap
+                            """)
+        foodwastes = cursor.fetchall()
+
+    db_conn.close() 
+
+    return foodwastes
+
+
